ce8ca0e1fc62882726f2bbe85073816e0257e19f
Patrice Nzenang <pnzenang@gmail.com>
2023-08-25 13:31:28 -0400

    modified:   client/src/pages/About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,12 @@
+import Wrapper from "../assets/wrappers/HomePage";
 import { Services, Contact } from "../components";
 
 const About = () => {
   return (
-    <>
+    <Wrapper>
       <Services />;
       <Contact />
-    </>
+    </Wrapper>
   );
 };
 export default About;
